cd827a175e5d2d0da94bdd2ee4b3d732a7a37dd9
Aldo Hernandez <aldohernandez@utez.edu.mx>
2022-03-07 00:56:51 -0600

    Project created

---
+++ src/test/java/mx/edu/utez/neighborhoodcommitte/NeighborhoodCommitteApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.neighborhoodcommitte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighborhoodCommitteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
